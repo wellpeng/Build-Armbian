a1c5c53445037dfe11a9fe3b97e303aa7f554124
balbes150 <balbes-150@yandex.ru>
2019-07-05 10:26:05 +0300

    v20190703

--- lib/configuration.sh
+++ lib/configuration.sh
@@ -12,7 +12,7 @@
 # common options
 # daily beta build contains date in subrevision
 if [[ $BETA == yes && -z $SUBREVISION ]]; then SUBREVISION="."$(date --date="tomorrow" +"%y%m%d"); fi
-REVISION="5.89$SUBREVISION" # all boards have same revision
+REVISION="5.90$SUBREVISION" # all boards have same revision
 ROOTPWD="1234" # Must be changed @first login
 [[ -z $MAINTAINER ]] && MAINTAINER="Oleg Ivanov" # deb signature
 [[ -z $MAINTAINERMAIL ]] && MAINTAINERMAIL="balbes-150@yandex.ru" # deb signature
@@ -70,7 +70,7 @@ CAN_BUILD_STRETCH=yes
 [[ -z $CRYPTROOT_SSH_UNLOCK ]] && CRYPTROOT_SSH_UNLOCK=yes
 [[ -z $CRYPTROOT_SSH_UNLOCK_PORT ]] && CRYPTROOT_SSH_UNLOCK_PORT=2022
 [[ -z $WIREGUARD ]] && WIREGUARD="no"
-[[ -z $RTL8812AU ]] && RTL8812AU="yes"
+[[ -z $EXTRAWIFI ]] && EXTRAWIFI="yes"
 
 #s8xx
 SRC_LOADADDR=""
